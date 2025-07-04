5cd61461da08bb09fc79632431da916852bddc25
HarvsG <11440490+HarvsG@users.noreply.github.com>
2025-07-04 13:40:54 +0000

    fix lint errors in merge

--- button.py
+++ button.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import logging
-from typing import Any
 
 from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
